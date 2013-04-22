f8f88f82bfe5e6169a728afece3494fa19821a3e
Mushegh Gevorgyan <mgevorgyan@macadamian.com>
2013-04-22 17:26:21 +0400

    DBIntegration

--- MacGym_WebRole/Controllers/WorkoutController.cs
+++ MacGym_WebRole/Controllers/WorkoutController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MacGym_DB;
+using MacGym_WebRole.Models;
 
 namespace MacGym_WebRole.Controllers
 {
@@ -10,7 +12,10 @@ namespace MacGym_WebRole.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            IndexModel model = new IndexModel();
+            model.Tools = new ToolsRepository().Get();
+            model.BodyParts = new BodyPartsRepository().Get();
+            return View(model);
         }
 
         public ActionResult Details()
---
+++ MacGym_WebRole/Models/ToolModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MacGym_DB;
+
+namespace MacGym_WebRole.Models
+{
+    public class IndexModel
+    {
+        public List<Tool> Tools { get; set; }
+        public List<BodyPart> BodyParts { get; set; }
+    }
+}
